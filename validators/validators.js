f29680b2ddc1a2d8fc56b7293b240d0481e2bb7f
Maddy <mmcintosh2k14@gmail.com>
2019-12-06 12:59:38 -0500

    creates action validator

--- validators/validators.js
+++ validators/validators.js
@@ -3,7 +3,8 @@ const Projects = require('../data/helpers/projectModel');
 
 module.exports = {
     validateProjectId,
-    validateProject
+    validateProject,
+    validateAction
 }
 
 //to be used on every GET request that expects a project id parameter.
@@ -33,6 +34,24 @@ function validateProject(req, res, next) {
     }
     if (!name || !description) {
         res.status(400).json({ message: 'Missing required name and/or description field.' });
+    } else {
+        next();
+    }
+};
+
+// validates the body on a POST request to create a new action for a project with specified ID
+function validateAction(req, res, next) {
+    const actionData = { ...req.body, project_id: req.params.id };
+    const { description, notes } = actionData;
+
+    if (Object.keys(actionData).length ===0) {
+        res.status(400).json({ message: 'missing action info.' });
+    }
+    if (description > 128) {
+        res.status(400).json({ message: "Description can't be longer than 128 characters." })
+    }
+    if (!description || !notes) {
+        res.status(400).json({ message: 'Missing required description and/or notes field.' })
     } else {
         next();
     }
