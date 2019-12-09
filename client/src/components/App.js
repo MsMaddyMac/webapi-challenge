51df38d65b65bb6df6b8dc730a1bd54b6b76f095
Maddy <mmcintosh2k14@gmail.com>
2019-12-09 11:35:52 -0500

    reconfigures file structure

---
+++ client/src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import ProjectList from './ProjectList';
+
+
+
+
+const App = () => {
+  
+
+  return (
+    <div>
+      <h2>Beginning of Project Board!</h2>
+      {/* <Route exact path="/" component={ProjectList} /> */}
+      {/* <Route path="/projects/:id" 
+        render={(props)=> 
+        <Project />}
+        /> */}
+    </div>
+  );
+};
+
+export default App;
+
